5fdda8a1f5ba35e38a91ebd4d84c135cae39e5b1
Aubry <willain.aubry@outlook.be>
2021-02-23 17:07:33 +0100

    VERSION A 0.0.5
    Creation ProductService

--- src/main/java/be/technifutur/Labo3/model/repositories/OrderRepository.java
+++ src/main/java/be/technifutur/Labo3/model/repositories/OrderRepository.java
@@ -1,7 +1,13 @@
 package be.technifutur.Labo3.model.repositories;
 
 import be.technifutur.Labo3.model.entities.Order;
+import be.technifutur.Labo3.model.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order,Integer> {
+
+    List<Order> findAllByProduct(Product product);
+
 }
--- src/main/java/be/technifutur/Labo3/model/services/ProductService.java
+++ src/main/java/be/technifutur/Labo3/model/services/ProductService.java
@@ -3,21 +3,28 @@ package be.technifutur.Labo3.model.services;
 import be.technifutur.Labo3.mapper.Mapper;
 import be.technifutur.Labo3.model.dtos.ProductDto;
 import be.technifutur.Labo3.model.entities.Product;
+import be.technifutur.Labo3.model.repositories.CategoryRepository;
+import be.technifutur.Labo3.model.repositories.OrderRepository;
 import be.technifutur.Labo3.model.repositories.ProductRepository;
+import be.technifutur.Labo3.model.repositories.SupplierRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService implements Crudable<Product, ProductDto, Integer> {
 
     private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
 
     private final Mapper mapper;
 
 
-    public ProductService(ProductRepository productRepository, Mapper mapper) {
+    public ProductService(ProductRepository productRepository, OrderRepository orderRepository, Mapper mapper) {
         this.productRepository = productRepository;
+        this.orderRepository = orderRepository;
         this.mapper = mapper;
     }
 
@@ -26,27 +33,63 @@ public class ProductService implements Crudable<Product, ProductDto, Integer> {
     public List<ProductDto> getAll() {
         return this.productRepository.findAll()
                 .stream()
-                .map(p -> mapper.to);
+                .map(p -> mapper.toProductDto(p, true))
+                .collect(Collectors.toList());
     }
 
     @Override
     public ProductDto getById(Integer integer) {
-        return null;
+
+        Product product = this.productRepository.findById(integer).orElseThrow(() -> new NoSuchElementException(("Product not found")));
+
+        product.setOrders(this.orderRepository.findAllByProduct(product));
+
+        return mapper.toProductDto(product, true);
     }
 
     @Override
     public boolean insert(Product product) {
-        return false;
+
+        Product newProduct = this.productRepository.save(product);
+
+        return this.productRepository.findById(newProduct.getProductId()).isPresent();
+
     }
 
     @Override
     public boolean update(Product product, Integer integer) {
-        return false;
+
+        Product oldProduct = this.productRepository.getOne(integer);
+
+        Product newProduct = new Product(oldProduct.getProductId(),
+                oldProduct.getName(),
+                oldProduct.getDescription(),
+                oldProduct.getCategories(),
+                oldProduct.getEntryDate(),
+                oldProduct.getUpdateDate(),
+                oldProduct.getExpirationDate(),
+                oldProduct.getPurchasePrice(),
+                oldProduct.getQuantity(),
+                oldProduct.getSupplier(),
+                oldProduct.getProductImage(),
+                oldProduct.getVat(),
+                oldProduct.getOrders());
+
+        product.setProductId(integer);
+
+        this.productRepository.save(product);
+
+        return !newProduct.equals(this.productRepository.getOne(integer));
+
     }
 
     @Override
     public boolean delete(Integer integer) {
-        return false;
+
+        this.productRepository.deleteById(integer);
+
+        return this.productRepository.findById(integer).isEmpty();
+
     }
 
 }
