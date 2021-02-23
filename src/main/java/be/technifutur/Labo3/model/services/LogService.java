330e8a54d465905d6850b768d9e7a0cb5abeaa5f
Jonathan <arabia.jonathan@hotmail.be>
2021-02-23 17:03:16 +0100

    Version 0.0.4.4
    logService

--- src/main/java/be/technifutur/Labo3/model/services/LogService.java
+++ src/main/java/be/technifutur/Labo3/model/services/LogService.java
@@ -7,6 +7,8 @@ import be.technifutur.Labo3.model.repositories.LogRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class LogService implements Crudable<Log, LogDto, Integer> {
@@ -21,26 +23,40 @@ public class LogService implements Crudable<Log, LogDto, Integer> {
 
     @Override
     public List<LogDto> getAll() {
-        return null;
+        return this.logRepository.findAll()
+                .stream()
+                .map(this.mapper::toLogDto)
+                .collect(Collectors.toList())
+                ;
     }
 
     @Override
     public LogDto getById(Integer integer) {
-        return null;
+        Log log = this.logRepository.findById(integer).orElseThrow(() -> new NoSuchElementException("Log not found"));
+        return this.mapper.toLogDto(log);
     }
 
     @Override
     public boolean insert(Log log) {
-        return false;
+        Log newLog = this.logRepository.save(log);
+        return this.logRepository.existsById(newLog.getLogId());
     }
 
     @Override
     public boolean update(Log log, Integer integer) {
-        return false;
+        Log old = this.logRepository.getOne(integer);
+        Log toTest = new Log(
+                old.getLogId(), old.getPrice(), old.getCreationDate(),
+                old.getProduct(), old.getUser()
+        );
+        log.setLogId(integer);
+        this.logRepository.save(log);
+        return !toTest.equals(this.logRepository.getOne(integer));
     }
 
     @Override
     public boolean delete(Integer integer) {
-        return false;
+        this.logRepository.deleteById(integer);
+        return this.logRepository.existsById(integer);
     }
 }
