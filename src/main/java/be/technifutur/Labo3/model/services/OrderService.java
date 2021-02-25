1daeca20a367419fa8e98476b1ac9a4b20e14e5e
Jonathan <arabia.jonathan@hotmail.be>
2021-02-25 08:44:58 +0100

    Version J 0.0.5.3
    Service Order

--- src/main/java/be/technifutur/Labo3/model/services/OrderService.java
+++ src/main/java/be/technifutur/Labo3/model/services/OrderService.java
@@ -7,6 +7,8 @@ import be.technifutur.Labo3.model.repositories.OrderRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService implements Crudable<Order, OrderDto, Integer> {
@@ -21,26 +23,49 @@ public class OrderService implements Crudable<Order, OrderDto, Integer> {
 
     @Override
     public List<OrderDto> getAll() {
-        return null;
+        return this.orderRepository.findAll()
+                .stream()
+                .map(o -> this.mapper.toOrderDto(o))
+                .collect(Collectors.toList())
+                ;
     }
 
     @Override
     public OrderDto getById(Integer integer) {
-        return null;
+        Order order = this.orderRepository.findById(integer).orElseThrow(() -> new NoSuchElementException("Order not found"));
+
+        return this.mapper.toOrderDto(order, true, true);
     }
 
     @Override
     public boolean insert(Order order) {
-        return false;
+        Order newOrder = this.orderRepository.save(order);
+        return this.orderRepository.existsById(newOrder.getOrderID());
     }
 
     @Override
     public boolean update(Order order, Integer integer) {
-        return false;
+        Order oldOrder = this.orderRepository.getOne(integer);
+        Order newOrder = new Order(
+                oldOrder.getOrderID(),
+                oldOrder.getReference(),
+                oldOrder.getCreationDate(),
+                oldOrder.getIsPaid(),
+                oldOrder.getPayementMethod(),
+                oldOrder.getUser(),
+                oldOrder.getProducts()
+        );
+
+        order.setOrderID(integer);
+
+        this.orderRepository.save(order);
+
+        return !newOrder.equals(this.orderRepository.getOne(integer));
     }
 
     @Override
     public boolean delete(Integer integer) {
-        return false;
+        this.orderRepository.deleteById(integer);
+        return this.orderRepository.existsById(integer);
     }
 }
