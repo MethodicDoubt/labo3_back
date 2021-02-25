6a64ae0224b34c4c7daabc3475f4407270f137c2
Jonathan <arabia.jonathan@hotmail.be>
2021-02-25 10:24:21 +0100

    Version J 0.0.6.2
    RestContrallable + ProductController

--- src/main/java/be/technifutur/Labo3/controllers/ProductController.java
+++ src/main/java/be/technifutur/Labo3/controllers/ProductController.java
@@ -1,12 +1,16 @@
 package be.technifutur.Labo3.controllers;
 
+import be.technifutur.Labo3.model.dtos.ProductDto;
+import be.technifutur.Labo3.model.entities.Product;
 import be.technifutur.Labo3.model.services.ProductService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("products")
-public class ProductController {
+public class ProductController implements RestControllable<Product, ProductDto, Integer> {
 
     private final ProductService productService;
 
@@ -15,4 +19,30 @@ public class ProductController {
     }
 
 
+    @Override
+    public ResponseEntity<List<ProductDto>> getAll() {
+        return null;
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getOne(@PathVariable("id") Integer integer) {
+        return ResponseEntity.ok(this.productService.getById(integer));
+    }
+
+    @Override
+    public ResponseEntity<Boolean> insert(Product product) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("/{id}")
+    public ResponseEntity<Boolean> update(Product product,@PathVariable("id") Integer integer) {
+        return ResponseEntity.ok(this.productService.update(product,integer));
+    }
+
+    @Override
+    public ResponseEntity<Boolean> delete(Integer integer) {
+        return null;
+    }
 }
---
+++ src/main/java/be/technifutur/Labo3/controllers/RestControllable.java
@@ -0,0 +1,19 @@
+package be.technifutur.Labo3.controllers;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+
+public interface RestControllable<ENTITY, DTO, ID> {
+
+    ResponseEntity<List<DTO>> getAll();
+
+    ResponseEntity<DTO> getOne(ID id);
+
+    ResponseEntity<Boolean> insert(ENTITY entity);
+
+    ResponseEntity<Boolean> update(ENTITY entity, ID id);
+
+    ResponseEntity<Boolean> delete(ID id);
+}
