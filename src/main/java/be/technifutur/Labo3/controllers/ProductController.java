751e32b5c78ef127a9720ce6c1db5943a81d5d26
Jonathan <arabia.jonathan@hotmail.be>
2021-02-25 10:18:33 +0100

    Version J 0.0.6.1
    Little changes Controller

--- src/main/java/be/technifutur/Labo3/controllers/ProductController.java
+++ src/main/java/be/technifutur/Labo3/controllers/ProductController.java
@@ -1,8 +1,18 @@
 package be.technifutur.Labo3.controllers;
 
+import be.technifutur.Labo3.model.services.ProductService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
 
 }
